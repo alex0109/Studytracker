3745c8ca380f10319007a80904f04d071eb1d8a1
slipokurov <oleksii.sli@cgsteam.io>
2025-07-23 19:18:44 +0200

    init

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Synaesthetic",
+  description: "Cool app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="relative bg-gray-700">
+        <div
+          className="bg-[#f6e2fb] absolute top-[-6rem] 
+          -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] 
+          rounded-full blur-[10rem] sm:w-[68.75rem]"
+        ></div>
+        <div
+          className="bg-[#e8d7fb] absolute top-[-1rem] 
+          -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full 
+          blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] 
+          lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
+        ></div>
+        {children}
+      </body>
+    </html>
+  );
+}
