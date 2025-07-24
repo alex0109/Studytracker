a53338c915f0c355bf401f76295ccbe742268f05
alex0109 <krossyouyub@gmail.com>
2025-07-24 09:08:56 +0200

    feat: Added chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,17 +13,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="relative bg-gray-700">
+      <body className="relative bg-gray-50">
         <div
           className="bg-[#f6e2fb] absolute top-[-6rem] 
           -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] 
-          rounded-full blur-[10rem] sm:w-[68.75rem]"
+          rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"
         ></div>
         <div
           className="bg-[#e8d7fb] absolute top-[-1rem] 
           -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full 
           blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] 
-          lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
+          lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"
         ></div>
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Intro from "./components/intro";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col items-center pt-10">
+      <Intro />
+    </main>
+  );
 }
