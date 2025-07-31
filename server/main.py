4c8fb919f4eb6f422cc6bbacab9b577176e3d1cd
Reybey <krossyouyub@gmail.com>
2025-07-31 19:21:41 +0200

    feat: cors

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from data import materials
 from pydantic import BaseModel
 import random
@@ -13,6 +14,14 @@ class MaterialModel(BaseModel):
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def __app__():
