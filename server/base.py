e1dc44e1c5bc14ad1ff34ab0af62d3536a166db7
Reybey <krossyouyub@gmail.com>
2025-09-03 23:31:23 +0200

    feat: docker set up

--- server/base.py
+++ server/base.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_URL = "sqlite:///./database.db"
+DB_URL = "sqlite:///./db/database.db"
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
 Base = declarative_base()
